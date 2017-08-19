30691c346873993370d5f7442f08127b5b996116
Jason Lu <syscallrax@gmail.com>
2017-08-18 23:24:47 -0500

    Add cpu_fetch_data_byte

--- hw/cpu/memory.c
+++ hw/cpu/memory.c
@@ -1,2 +1,10 @@
 #include <board.h>
+#include <hw/chipset/memory.h>
+#include <hw/cpu/cpu.h>
 #include <hw/cpu/memory.h>
+
+uint8_t cpu_fetch_data_byte(struct cpu* cpu, addr_t offset)
+{
+    addr_t final = (cpu->ds << 4) + offset;
+    return memory_read_byte(cpu->board, final);
+}
