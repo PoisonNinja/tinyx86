8e324cbe36e3fc5a37b2c423e095f264075f75a5
Jason Lu <syscallrax@gmail.com>
2017-08-19 23:11:22 -0500

    Add opcode infrastructure

--- hw/cpu/opcode.c
+++ hw/cpu/opcode.c
@@ -2,7 +2,53 @@
 #include <hw/cpu/memory.h>
 #include <hw/cpu/opcode.h>
 
+typedef void (*opcode_fn_t)(struct cpu* cpu);
+
+#define OPCODE_DEFINE(number) static void opcode##number(struct cpu* cpu)
+#define OPCODE_ENTRY(number) &opcode##number
+
+static inline void raw_to_modrm(uint8_t raw, struct modrm* modrm)
+{
+    uint8_t* tmp = (uint8_t*)modrm;
+    *tmp = raw;
+}
+
+opcode_fn_t opcode_table[256] = {
+    NULL, NULL, NULL, NULL, NULL,     NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL,     NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL,     NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL,     NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL,     NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL,     NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL,     NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL,     NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL,     NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL,     NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL,     NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL,     NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL,     NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL,     NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL,     NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL,     NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL,     NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL,     NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL,     NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL,     NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL,     NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL,
+};
+
 void opcode_execute(struct cpu* cpu)
 {
     uint8_t opcode = cpu_fetch_instruction_byte(cpu);
+    if (opcode == 0x0F) {
+        log_fatal("We do not support two byte opcodes yet!");
+    }
+    log_trace("Opcode: %X", opcode);
+    if (!opcode_table[opcode]) {
+        log_fatal("Unknown opcode, should probably throw an Invalid Opcode "
+                  "exception, got %X",
+                  opcode);
+    }
+    opcode_table[opcode](cpu);
 }
--- include/hw/cpu/opcode.h
+++ include/hw/cpu/opcode.h
@@ -2,4 +2,16 @@
 
 struct cpu;
 
+struct modrm {
+    uint8_t rm : 3;
+    uint8_t reg : 3;
+    uint8_t mod : 2;
+} __attribute__((packed));
+
+struct sib {
+    uint8_t base : 3;
+    uint8_t index : 3;
+    uint8_t scale : 2;
+} __attribute__((packed));
+
 extern void opcode_execute(struct cpu* cpu);
