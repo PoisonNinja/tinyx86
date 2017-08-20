6f4a80e081c13f1fa036a324b67817e7c8a9ae88
Jason Lu <syscallrax@gmail.com>
2017-08-19 22:59:32 -0500

    Add hello source code and binary

---
+++ test/hello.asm
@@ -0,0 +1,39 @@
+start:
+	mov ax, msg
+	call print
+	hlt
+
+print:
+        push bx
+        push dx
+        mov bx, ax
+
+print_loop:
+        mov dl, [bx]
+        inc bx
+        xchg al, dl
+        call print_char
+        xchg al, dl
+        and dl, dl
+        jnz print_loop
+        pop dx
+        pop bx
+        ret
+
+print_char:
+        push bx
+        push di
+        mov bx, 8000h
+        mov di, [current]
+        mov [bx + di], al
+        inc di
+        mov [current], di
+        pop di
+        pop bx
+        ret
+
+msg:
+	db      "Hello, CSO!", 0
+
+current:
+	dw	0
