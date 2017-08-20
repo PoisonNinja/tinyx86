694b6780d4bb53b01fba1e68d3a3d51198a7f49b
Jason Lu <syscallrax@gmail.com>
2017-08-19 23:08:46 -0500

    Add cpu_fetch_*_word family

--- hw/cpu/memory.c
+++ hw/cpu/memory.c
@@ -12,8 +12,22 @@ uint8_t cpu_fetch_instruction_byte(struct cpu* cpu)
     return ret;
 }
 
+uint16_t cpu_fetch_instruction_word(struct cpu* cpu)
+{
+    addr_t final = cpu->cs.base + cpu->ip.regs_16;
+    uint8_t ret = memory_read_word(cpu->board, final);
+    cpu->ip.regs_16 += 2;
+    return ret;
+}
+
 uint8_t cpu_fetch_data_byte(struct cpu* cpu, addr_t offset)
 {
     addr_t final = cpu->ds.base + offset;
     return memory_read_byte(cpu->board, final);
+}
+
+uint16_t cpu_fetch_data_word(struct cpu* cpu, addr_t offset)
+{
+    addr_t final = cpu->ds.base + offset;
+    return memory_read_word(cpu->board, final);
 }
--- include/hw/cpu/memory.h
+++ include/hw/cpu/memory.h
@@ -3,4 +3,7 @@
 #include <x86emu.h>
 
 extern uint8_t cpu_fetch_instruction_byte(struct cpu* cpu);
+extern uint16_t cpu_fetch_instruction_word(struct cpu* cpu);
+
 extern uint8_t cpu_fetch_data_byte(struct cpu* cpu, addr_t offset);
+extern uint16_t cpu_fetch_data_word(struct cpu* cpu, addr_t offset);
