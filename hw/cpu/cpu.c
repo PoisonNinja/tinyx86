ca842e5a342be2cfe2a63a5fbbde51508bd13c0a
Jason Lu <syscallrax@gmail.com>
2017-08-20 23:36:21 -0500

    Memset CPU when resetting

--- hw/cpu/cpu.c
+++ hw/cpu/cpu.c
@@ -12,6 +12,9 @@ void cpu_cycle(struct cpu* cpu)
 
 void cpu_reset(struct cpu* cpu)
 {
+    struct board* board = cpu->board;
+    memset(cpu, 0, sizeof(struct cpu));
+    cpu->board = board;
     // cpu->cs.selector = 0xF000;
     // cpu->cs.base = 0xFFFF0000;
     // cpu->cs.limit = 0xFFFF;
