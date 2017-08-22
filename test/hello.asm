6f3f580b4509402e30c5bf7489babef102ae68b0
Jason Lu <syscallrax@gmail.com>
2017-08-21 22:38:13 -0500

    Format ASM file properly

--- test/hello.asm
+++ test/hello.asm
@@ -1,41 +1,41 @@
 org 0x0
 start:
-	mov sp, 0x2000
-	mov ax, msg
-	call print
-	hlt
+    mov sp, 0x2000
+    mov ax, msg
+    call print
+    hlt
 
 print:
-        push bx
-        push dx
-        mov bx, ax
+    push bx
+    push dx
+    mov bx, ax
 
 print_loop:
-        mov dl, [bx]
-        inc bx
-        xchg al, dl
-        call print_char
-        xchg al, dl
-        and dl, dl
-        jnz print_loop
-        pop dx
-        pop bx
-        ret
+    mov dl, [bx]
+    inc bx
+    xchg al, dl
+    call print_char
+    xchg al, dl
+    and dl, dl
+    jnz print_loop
+    pop dx
+    pop bx
+    ret
 
 print_char:
-        push bx
-        push di
-        mov bx, 8000h
-        mov di, [current]
-        mov [bx + di], al
-        inc di
-        mov [current], di
-        pop di
-        pop bx
-        ret
+    push bx
+    push di
+    mov bx, 8000h
+    mov di, [current]
+    mov [bx + di], al
+    inc di
+    mov [current], di
+    pop di
+    pop bx
+    ret
 
 msg:
-	db      "Hello, CSO!", 0
+    db      "Hello, CSO!", 0
 
 current:
-	dw	0
+    dw	0
