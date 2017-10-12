203f1b5821090dba88abad710692101a6db6de6a
Jason Lu <syscallrax@gmail.com>
2017-10-12 11:00:18 -0700

    Properly check memory region bounds

--- hw/chipset/memory.c
+++ hw/chipset/memory.c
@@ -17,16 +17,17 @@ uint8_t memory_read_byte(struct board* board, addr_t addr)
     struct memory_region* rom = NULL;
     list_for_each(&board->memory->rom_regions, list, rom)
     {
-        if (rom->base <= addr && rom->size - rom->base >= 1)
+        if (rom->base <= addr && rom->base + rom->size >= addr + 1)
             return rom_read_byte(rom, addr);
     }
     struct memory_region* ram = NULL;
     list_for_each(&board->memory->ram_regions, list, ram)
     {
-        if (ram->base <= addr && ram->size - ram->base >= 1)
+        if (ram->base <= addr && ram->base + ram->size >= addr + 1)
             return ram_read_byte(ram, addr);
     }
-    return 0x0;
+    log_fatal("Attempting to read outside memory: %p", addr);
+    tinyx86_exit(1);
 }
 
 void memory_write_byte(struct board* board, addr_t addr, uint8_t value)
@@ -34,14 +35,17 @@ void memory_write_byte(struct board* board, addr_t addr, uint8_t value)
     struct memory_region* rom = NULL;
     list_for_each(&board->memory->rom_regions, list, rom)
     {
-        if (rom->base <= addr && rom->size - rom->base >= 1)
+        if (rom->base <= addr && rom->base + rom->size >= addr + 1)
             return rom_write_byte(rom, addr, value);
     }
     struct memory_region* ram = NULL;
     list_for_each(&board->memory->ram_regions, list, ram)
     {
-        return ram_write_byte(ram, addr, value);
+        if (ram->base <= addr && ram->base + ram->size >= addr + 1)
+            return ram_write_byte(ram, addr, value);
     }
+    log_fatal("Attempting to write outside memory: %p", addr);
+    tinyx86_exit(1);
 }
 
 uint16_t memory_read_word(struct board* board, addr_t addr)
@@ -49,16 +53,17 @@ uint16_t memory_read_word(struct board* board, addr_t addr)
     struct memory_region* rom = NULL;
     list_for_each(&board->memory->rom_regions, list, rom)
     {
-        if (rom->base <= addr && rom->size - rom->base >= 2)
+        if (rom->base <= addr && rom->base + rom->size >= addr + 2)
             return rom_read_word(rom, addr);
     }
     struct memory_region* ram = NULL;
     list_for_each(&board->memory->ram_regions, list, ram)
     {
-        if (ram->base <= addr && ram->size - ram->base >= 2)
+        if (ram->base <= addr && ram->base + ram->size >= addr + 2)
             return ram_read_byte(ram, addr);
     }
-    return 0;
+    log_fatal("Attempting to read outside memory: %p", addr);
+    tinyx86_exit(1);
 }
 
 void memory_write_word(struct board* board, addr_t addr, uint16_t value)
@@ -66,12 +71,15 @@ void memory_write_word(struct board* board, addr_t addr, uint16_t value)
     struct memory_region* rom = NULL;
     list_for_each(&board->memory->rom_regions, list, rom)
     {
-        if (rom->base <= addr && rom->size - rom->base >= 2)
+        if (rom->base <= addr && rom->base + rom->size >= addr + 2)
             return rom_write_word(rom, addr, value);
     }
     struct memory_region* ram = NULL;
     list_for_each(&board->memory->ram_regions, list, ram)
     {
-        return ram_write_word(ram, addr, value);
+        if (ram->base <= addr && ram->base + ram->size >= addr + 2)
+            return ram_write_word(ram, addr, value);
     }
+    log_fatal("Attempting to write outside memory: %p", addr);
+    tinyx86_exit(1);
 }
