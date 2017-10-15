fc69fdf0f6702b31c4451eba1ca6c9b6d7f88b8d
Jason Lu <syscallrax@gmail.com>
2017-10-15 13:22:35 -0700

    Set window alpha to 255

--- gui/window.c
+++ gui/window.c
@@ -10,7 +10,7 @@ void window_create(size_t x, size_t y)
     SDL_Window *window;
     SDL_Renderer *renderer;
     SDL_CreateWindowAndRenderer(x, y, 0, &window, &renderer);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
     SDL_RenderPresent(renderer);
 }
