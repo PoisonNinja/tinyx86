c4f123b5e254d53010c21930edc7613b0b76844a
Jason Lu <syscallrax@gmail.com>
2018-07-10 19:59:36 -0500

    Revert "Remove test binary"
    This reverts commit c5c31e670393ace03c4f4885ae02c956aad95d5a.

---
+++ test/hello.asm
@@ -0,0 +1,41 @@
+org 0x0
+start:
+    mov sp, 0x2000
+    mov ax, msg
+    call print
+    hlt
+
+print:
+    push bx
+    push dx
+    mov bx, ax
+
+print_loop:
+    mov dl, [bx]
+    inc bx
+    xchg al, dl
+    call print_char
+    xchg al, dl
+    and dl, dl
+    jnz print_loop
+    pop dx
+    pop bx
+    ret
+
+print_char:
+    push bx
+    push di
+    mov bx, 8000h
+    mov di, [current]
+    mov [bx + di], al
+    inc di
+    mov [current], di
+    pop di
+    pop bx
+    ret
+
+msg:
+    db "Hello, world!", 0
+
+current:
+    dw	0
