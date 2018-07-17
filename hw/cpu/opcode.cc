d234ab39c5190ee3f39e0a56b1360c15808d2628
Jason Lu <syscallrax@gmail.com>
2018-07-16 20:30:15 -0500

    hw: cpu: opcode: Remove logging

--- hw/cpu/opcode.cc
+++ hw/cpu/opcode.cc
@@ -3,68 +3,59 @@
 
 void InstructionDecoder::push_dx()
 {
-    this->log->trace("push dx");
     this->cpu.push16(this->cpu.read_gpreg16(GPRegister16::DX));
 }
 
 void InstructionDecoder::push_edx()
 {
-    this->log->trace("push edx");
     this->cpu.push32(this->cpu.read_gpreg32(GPRegister32::EDX));
 }
 
 void InstructionDecoder::push_bx()
 {
-    this->log->trace("push bx");
     this->cpu.push16(this->cpu.read_gpreg16(GPRegister16::BX));
 }
 
 void InstructionDecoder::push_ebx()
 {
-    this->log->trace("push ebx");
     this->cpu.push32(this->cpu.read_gpreg32(GPRegister32::EBX));
 }
 
 void InstructionDecoder::mov_ax_imm16()
 {
-    uint16_t val = this->cpu.read_instruction16();
-    this->log->trace("mov ax, 0x{:X}", val);
-    this->cpu.write_gpreg16(GPRegister16::AX, val);
+    this->cpu.write_gpreg16(GPRegister16::AX, this->cpu.read_instruction16());
 }
 
 void InstructionDecoder::mov_eax_imm32()
 {
-    uint32_t val = this->cpu.read_instruction32();
-    this->log->trace("mov eax, 0x{:X}", val);
-    this->cpu.write_gpreg32(GPRegister32::EAX, val);
+    this->cpu.write_gpreg32(GPRegister32::EAX, this->cpu.read_instruction32());
 }
 
 void InstructionDecoder::mov_sp_imm16()
 {
-    uint16_t val = this->cpu.read_instruction16();
-    this->log->trace("mov sp, 0x{:X}", val);
-    this->cpu.write_gpreg16(GPRegister16::SP, val);
+    this->cpu.write_gpreg16(GPRegister16::SP, this->cpu.read_instruction16());
 }
 
 void InstructionDecoder::mov_esp_imm32()
 {
-    uint32_t val = this->cpu.read_instruction32();
-    this->log->trace("mov esp, 0x{:X}", val);
-    this->cpu.write_gpreg32(GPRegister32::ESP, val);
+    this->cpu.write_gpreg32(GPRegister32::ESP, this->cpu.read_instruction32());
 }
 
 void InstructionDecoder::call_rel16()
 {
-    int16_t imm16 = this->cpu.read_instruction16();
-    this->log->trace("call 0x{:X} (rel16)", imm16);
+    /*
+     * Order matters! Do not try to move the imm fetch into the write_ip
+     * call because then we will push the wrong address onto the stack to
+     * return to.
+     */
+    uint16_t imm = this->cpu.read_instruction16();
     this->cpu.push16(this->cpu.read_ip());
-    this->cpu.write_ip(this->cpu.read_ip() + imm16);
+    this->cpu.write_ip(this->cpu.read_ip() + imm);
 }
 
 void InstructionDecoder::call_rel32()
 {
-    int32_t imm32 = this->cpu.read_instruction32();
-    this->log->trace("call 0x{:X} (rel32)", imm32);
+    uint32_t imm = this->cpu.read_instruction32();
     this->cpu.push32(this->cpu.read_eip());
-    this->cpu.write_eip(this->cpu.read_eip() + imm32);
+    this->cpu.write_eip(this->cpu.read_eip() + imm);
 }
