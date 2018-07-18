d97034ccd3342ef1903705d7ed710532f04cbcd7
Jason Lu <syscallrax@gmail.com>
2018-07-18 00:16:20 -0500

    hw: cpu: Convert EFLAGS into member of CPU instead of seperate class

--- hw/cpu/cpu.cc
+++ hw/cpu/cpu.cc
@@ -1,139 +1,6 @@
 #include <hw/board.h>
 #include <hw/cpu/cpu.h>
 
-EFLAGS::EFLAGS()
-{
-}
-
-EFLAGS::~EFLAGS()
-{
-}
-
-void EFLAGS::reset()
-{
-    this->eflags = 0;
-    this->eflags_dirty = 0;
-    this->last_op1 = 0;
-    this->last_op2 = 0;
-    this->last_result = 0;
-    this->last_add_result = 0;
-    this->last_size = 0;
-}
-
-uint32_t EFLAGS::get_eflags()
-{
-    get_cf();
-    get_pf();
-    get_af();
-    get_zf();
-    get_sf();
-    get_of();
-    return eflags;
-}
-
-/*
- * The following flag calculations are largely borrowed from v86 written by
- * Fabian Hemmer. The code is licensed under the Simplified BSD license.
- * It has been reproduced here:
- *
- * Copyright (c) 2012-2014, Fabian Hemmer
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
- * The views and conclusions contained in the software and documentation are
- * those of the authors and should not be interpreted as representing official
- * policies, either expressed or implied, of the FreeBSD Project.
- */
-
-bool EFLAGS::get_cf()
-{
-    if (eflags_dirty & eflags_cf) {
-        eflags |=
-            ((last_op1 ^ ((last_op1 ^ last_op2) & (last_op2 ^ last_result))) >>
-                 last_size &
-             1) ?
-                eflags_cf :
-                0;
-        eflags_dirty &= ~eflags_cf;
-    }
-    return eflags & eflags_cf;
-}
-
-bool EFLAGS::get_pf()
-{
-    if (eflags_dirty & eflags_pf) {
-        eflags |= (0x9669 << 2 >> ((last_result ^ last_result >> 4) & 0xF) &
-                   eflags_pf) ?
-                      eflags_pf :
-                      0;
-        eflags_dirty &= ~eflags_pf;
-    }
-    return eflags & eflags_pf;
-}
-
-bool EFLAGS::get_af()
-{
-    if (eflags_dirty & eflags_af) {
-        eflags |= ((last_op1 ^ last_op2 ^ last_add_result) & eflags_af) ?
-                      eflags_af :
-                      0;
-        eflags_dirty &= ~eflags_af;
-    }
-    return eflags & eflags_af;
-}
-
-bool EFLAGS::get_zf()
-{
-    if (eflags_dirty & eflags_zf) {
-        eflags |=
-            ((~last_result & last_result - 1) >> last_size & 1) ? eflags_zf : 0;
-        eflags_dirty &= ~eflags_zf;
-    }
-    return eflags & eflags_zf;
-}
-
-bool EFLAGS::get_sf()
-{
-    if (eflags_dirty & eflags_sf) {
-        eflags |= ((last_result >> last_size) & 1) ? eflags_sf : 0;
-        eflags_dirty &= ~eflags_sf;
-    }
-    return eflags & eflags_sf;
-}
-
-bool EFLAGS::get_of()
-{
-    if (eflags_dirty & eflags_of) {
-        eflags |=
-            (((last_op1 ^ last_add_result) & (last_op2 ^ last_add_result)) >>
-                 last_size &
-             1) ?
-                eflags_of :
-                0;
-    }
-    return eflags & eflags_of;
-}
-
 CPU::CPU(Board& b) : board(b), decoder(*this)
 {
     this->log = spdlog::get("stdout");
@@ -163,7 +30,15 @@ void CPU::reset()
         this->sgregs[i].limit = 0;
     }
     this->ip.regs_32 = 0;
-    this->eflags.reset();
+
+    // Reset EFLAGS
+    this->eflags = 0;
+    this->eflags_dirty = 0;
+    this->last_op1 = 0;
+    this->last_op2 = 0;
+    this->last_result = 0;
+    this->last_add_result = 0;
+    this->last_size = 0;
 
     // Set to reset vector
     this->ip.regs_32 = 0;
@@ -323,6 +198,136 @@ bool CPU::is_32()
 {
     // TODO: Eventually figure out of we are actually 32-bit based on GDT
     return false;
+}
+
+uint32_t CPU::get_eflags()
+{
+    get_cf();
+    get_pf();
+    get_af();
+    get_zf();
+    get_sf();
+    get_of();
+    return eflags;
+}
+
+/*
+ * The following flag calculations are largely borrowed from v86 written by
+ * Fabian Hemmer. The code is licensed under the Simplified BSD license.
+ * It has been reproduced here:
+ *
+ * Copyright (c) 2012-2014, Fabian Hemmer
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of the FreeBSD Project.
+ */
+
+bool CPU::get_cf()
+{
+    if (eflags_dirty & eflags_cf) {
+        eflags |=
+            ((last_op1 ^ ((last_op1 ^ last_op2) & (last_op2 ^ last_result))) >>
+                 last_size &
+             1) ?
+                eflags_cf :
+                0;
+        eflags_dirty &= ~eflags_cf;
+    }
+    return eflags & eflags_cf;
+}
+
+bool CPU::get_pf()
+{
+    if (eflags_dirty & eflags_pf) {
+        eflags |= (0x9669 << 2 >> ((last_result ^ last_result >> 4) & 0xF) &
+                   eflags_pf) ?
+                      eflags_pf :
+                      0;
+        eflags_dirty &= ~eflags_pf;
+    }
+    return eflags & eflags_pf;
+}
+
+bool CPU::get_af()
+{
+    if (eflags_dirty & eflags_af) {
+        eflags |= ((last_op1 ^ last_op2 ^ last_add_result) & eflags_af) ?
+                      eflags_af :
+                      0;
+        eflags_dirty &= ~eflags_af;
+    }
+    return eflags & eflags_af;
+}
+
+bool CPU::get_zf()
+{
+    if (eflags_dirty & eflags_zf) {
+        eflags |=
+            ((~last_result & last_result - 1) >> last_size & 1) ? eflags_zf : 0;
+        eflags_dirty &= ~eflags_zf;
+    }
+    return eflags & eflags_zf;
+}
+
+bool CPU::get_sf()
+{
+    if (eflags_dirty & eflags_sf) {
+        eflags |= ((last_result >> last_size) & 1) ? eflags_sf : 0;
+        eflags_dirty &= ~eflags_sf;
+    }
+    return eflags & eflags_sf;
+}
+
+bool CPU::get_of()
+{
+    if (eflags_dirty & eflags_of) {
+        eflags |=
+            (((last_op1 ^ last_add_result) & (last_op2 ^ last_add_result)) >>
+                 last_size &
+             1) ?
+                eflags_of :
+                0;
+    }
+    return eflags & eflags_of;
+}
+
+void CPU::set_eflag_operation(uint32_t eflags_dirty, uint32_t eflags_initial,
+                              uint32_t last_op1, uint32_t last_op2,
+                              uint32_t last_result, uint32_t last_add_result,
+                              uint8_t last_size)
+{
+    this->eflags &= this->eflags & 0xFFFFF72A;  // Clear all status flags
+    this->eflags |=
+        eflags_initial & 0x8D5;  // Set status flags based on initial
+    this->eflags_dirty = eflags_dirty;
+    this->last_op1 = last_op1;
+    this->last_op2 = last_op2;
+    this->last_result = last_result;
+    this->last_add_result = last_add_result;
+    this->last_size = last_size;
 }
 
 addr_t CPU::segment_to_linear(SGRegister reg, addr_t offset)
--- include/hw/cpu/cpu.h
+++ include/hw/cpu/cpu.h
@@ -36,32 +36,6 @@ constexpr uint32_t eflags_of = (1 << 11);
 constexpr uint32_t eflags_all =
     (eflags_cf | eflags_pf | eflags_af | eflags_zf | eflags_sf | eflags_of);
 
-class EFLAGS
-{
-public:
-    EFLAGS();
-    ~EFLAGS();
-
-    void reset();
-
-    uint32_t get_eflags();  // Gets EFLAGS and recomputes as necessary
-    bool get_cf();
-    bool get_pf();
-    bool get_af();
-    bool get_zf();
-    bool get_sf();
-    bool get_of();
-
-private:
-    uint32_t eflags;           // Internal representation of EFLAGS
-    uint32_t eflags_dirty;     // EFLAGS dirty status
-    uint32_t last_op1;         // Last operand #1
-    uint32_t last_op2;         // Last operand #2
-    uint32_t last_result;      // Result of last operation
-    uint32_t last_add_result;  // Result of last add operation
-    uint8_t last_size;         // Size of last operation
-};
-
 enum class GPRegister8 : uint8_t {
     AL = 0,
     CL,
@@ -162,6 +136,23 @@ public:
 
     bool is_32();
 
+    /*
+     * TODO: Is this actually a good solution? I experimented with moving this
+     * into a seperate class but it made accessing it from the
+     * InstructionDecoder unreasonably complicated.
+     */
+    uint32_t get_eflags();  // Gets EFLAGS and recomputes as necessary
+    bool get_cf();
+    bool get_pf();
+    bool get_af();
+    bool get_zf();
+    bool get_sf();
+    bool get_of();
+    void set_eflag_operation(uint32_t eflags_dirty, uint32_t eflags_initial,
+                             uint32_t last_op1, uint32_t last_op2,
+                             uint32_t last_result, uint32_t last_add_result,
+                             uint8_t last_size);
+
     addr_t segment_to_linear(SGRegister seg, addr_t offset);
 
     void halt();
@@ -187,5 +178,11 @@ private:
     Register ip;
 
     // EFLAGS
-    EFLAGS eflags;
+    uint32_t eflags;           // Internal representation of EFLAGS
+    uint32_t eflags_dirty;     // EFLAGS dirty status
+    uint32_t last_op1;         // Last operand #1
+    uint32_t last_op2;         // Last operand #2
+    uint32_t last_result;      // Result of last operation
+    uint32_t last_add_result;  // Result of last add operation
+    uint8_t last_size;         // Size of last operation
 };
